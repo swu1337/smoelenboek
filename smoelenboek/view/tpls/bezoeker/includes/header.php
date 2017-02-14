ff35f330985fa8f6dafb72940ecdee9b9189678e
Steven Wu <master_msp1@hotmail.com>
2017-02-15 00:15:11 +0100

    added meta tag

--- smoelenboek/view/tpls/bezoeker/includes/header.php
+++ smoelenboek/view/tpls/bezoeker/includes/header.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Smoelenboek</title>
         <link rel="stylesheet" href="css/normalize.css" type="text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
