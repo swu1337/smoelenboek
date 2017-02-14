b1e23fad202f31263f158566409bb82b4f40c690
Steven Wu <master_msp1@hotmail.com>
2017-02-14 23:05:47 +0100

    landing page for smoelenboek

--- smoelenboek/view/tpls/bezoeker/includes/header.php
+++ smoelenboek/view/tpls/bezoeker/includes/header.php
@@ -4,19 +4,60 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Smoelenboek</title>
         <link rel="stylesheet" href="css/normalize.css" type="text/css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <link rel="stylesheet" href="css/telefoonlijst.css" type="text/css">
         <link rel="stylesheet" href="css/bezoeker.css" type="text/css">
     </head>
     <body>
-        <header>
-            <figure>
-                <img src="img/mondriaan.jpg" alt="ons schoolgebouw aan de tinwerf 10 denhaag" />
-            </figure>
-            <div>
-                <p>Wij zijn een school voor ICT bestaande uit twee afdelingen.</p>
-                <p>De afdeling <em>ICT beheer</em> en de afdeling <em>Applicatieontwikkeling</em>. Onze school heeft ongeveer 30 collega's verdeeld over beide afdelingen</p>
-                <p>De school heeft als manager <a href="?control=bezoeker&action=directeur"><em><?= $directeur->getNaam();?></em></a>
-                    <?= isset($boodschap)?"<p id = 'boodschap'><em>$boodschap</em></p>":""?>
+        <header class="header__container">
+            <div class="d-flex align-items-center flex-column header__items">
+                <h1 class="display-4 text-white text-center">Welcome bij het Smoelenboek</h1>
+                <p class="lead text-white">Smoelenboek van Mondriaan ICT</p>
+                <button type="button" class="btn btn-primary btn-lg">Login</button>
             </div>
         </header>
-        <section>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container-fluid">
+            <h1 class="display-3">Fluid jumbotron</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
+
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Login</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Use Your Credential To Login</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+          <div class="container">
+  <form action="#">
+    <div class="form-group row">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+      </div>
+    </div>
+    <div class="form-group row">
+      <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      <button type="submit" class="btn btn-primary">Save changes</button>
+    </div>
+
+</div>
+    </div>
+    </div>
+  </div>
+</div>
