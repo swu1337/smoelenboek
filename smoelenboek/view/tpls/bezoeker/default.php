3ecbfa623909b340559da8d036c07eb9a4ba9f60
Steven Wu <master_msp1@hotmail.com>
2017-02-16 20:40:55 +0100

    fixed footer

--- smoelenboek/view/tpls/bezoeker/default.php
+++ smoelenboek/view/tpls/bezoeker/default.php
@@ -2,8 +2,8 @@
 <div class="container">
     <div class="row">
         <div class="col-md-7">
-            <h2 class="display-4 card__heading pt-5">Applicatie- en Mediaontwikkelaar.<span class="text-muted"> ❖ Yunus is the best kappa </span></h2>
-            <p class="lead">Als applicatieontwikkelaar ontwikkel je nieuwe software en pas je bestaande software aan. Je doorziet snel problemen en je vertaalt deze naar een ICT-oplossing. Je kunt gebruikers mondeling en schriftelijk informeren en helpen bij voorkomende problemen. Bij veel van je werkzaamheden is haast geboden. Werken onder tijdsdruk mag dus geen probleem zijn. Je bent getraind in planmatig werken. Je bent flexibel als het gaat om werktijden en het gebruik van diverse ontwikkel omgevingen.</p>
+            <h2 class="display-4 card__heading pt-5">Applicatie- en Mediaontwikkelaar.<span class="text-muted"> ❖ BOL 3jaar </span></h2>
+            <p class="lead pt-4 text-justify">Als applicatieontwikkelaar ontwikkel je nieuwe software en pas je bestaande software aan. Je doorziet snel problemen en je vertaalt deze naar een ICT-oplossing. Je kunt gebruikers mondeling en schriftelijk informeren en helpen bij voorkomende problemen. Bij veel van je werkzaamheden is haast geboden. Werken onder tijdsdruk mag dus geen probleem zijn. Je bent getraind in planmatig werken. Je bent flexibel als het gaat om werktijden en het gebruik van diverse ontwikkel omgevingen.</p>
         </div>
         <div class="col-md-5">
             <img class="img-fluid card__image" data-src="holder.js/500x500/auto" alt="500x500" src="http://static.rocmondriaan.nl/assets/83e98702b645cd8e35314fd721d1a532/201557_IF_Applicatieontwikkelaar_01.jpg" data-holder-rendered="true">
@@ -15,15 +15,15 @@
             <img class="img-fluid card__image" data-src="holder.js/500x500/auto" alt="500x500" src="http://www.rocmondriaan.nl/image/650?url=http%3A%2F%2Fstatic.rocmondriaan.nl%2Fassets%2Ffd9c6016e12af35d5bbf1883922330cd%2F110849_DSC_0231%20-%202012-05-14%20at%2011-14-47.jpg" data-holder-rendered="true">
         </div>
         <div class="col-md-7">
-            <h2 class="display-4 card__heading pt-5">ICT.<span class="text-muted"> ❖ It'll blow your mind.</span></h2>
-            <p class="lead">In de industrie en op kantoor zijn de computer, het netwerk, de nodige besturingssystemen en software onmisbaar. De ICT- en Netwerkbeheerder zorgen er voor dat alles blijft draaien. Je helpt gebruikers bij problemen en je adviseert het management over de in te zetten informatiesystemen. Ga je voor Applicatieontwikkelaar, dan programmeer je allerlei voor de bedrijfsvoering noodzakelijke software.</p>
+            <h2 class="display-4 card__heading pt-5">School voor ICT.<span class="text-muted"> ❖ Tinwerf 16 </span></h2>
+            <p class="lead pt-4 text-justify">In de industrie en op kantoor zijn de computer, het netwerk, de nodige besturingssystemen en software onmisbaar. De ICT- en Netwerkbeheerder zorgen er voor dat alles blijft draaien. Je helpt gebruikers bij problemen en je adviseert het management over de in te zetten informatiesystemen. Ga je voor Applicatieontwikkelaar, dan programmeer je allerlei voor de bedrijfsvoering noodzakelijke software.</p>
         </div>
     </div>
     <div class="divider"></div>
     <div class="row mb-5">
         <div class="col-md-7">
-            <h2 class="display-4 card__heading pt-5">Netwerk- en Mediabeheer<span class="text-muted"> ❖ It'll blow your mind.</span></h2>
-            <p class="lead">Je houdt je vooral bezig met ontwerp, implementatie en onderhoud van middelgrote netwerksystemen. Netwerken beheren en de beveiliging daarvan behoren tot je kerntaken. Je bent bekend met de modernste communicatiemiddelen. Je werkt met besturings- en toepassingssoftware. Ook leer je van alles over intranet en internet.</p>
+            <h2 class="display-4 card__heading pt-5">Netwerk- en Mediabeheer<span class="text-muted"> ❖ BOL 3jaar</span></h2>
+            <p class="lead pt-4 text-justify">Je houdt je vooral bezig met ontwerp, implementatie en onderhoud van middelgrote netwerksystemen. Netwerken beheren en de beveiliging daarvan behoren tot je kerntaken. Je bent bekend met de modernste communicatiemiddelen. Je werkt met besturings- en toepassingssoftware. Ook leer je van alles over intranet en internet.</p>
         </div>
         <div class="col-md-5">
             <img class="img-fluid card__image" data-src="holder.js/500x500/auto" alt="500x500" src="http://www.rocmondriaan.nl/image/700?url=http%3A%2F%2Fstatic.rocmondriaan.nl%2Fassets%2F9bdd640bb01c4521cf76a6739ad23d3b%2F114317_DSC_0212%20-%202012-05-14%20at%2011-11-34.jpg" data-holder-rendered="true">
