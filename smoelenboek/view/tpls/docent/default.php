95ca0f8c8c54b86941a36955209c4dac4e685877
Steven Wu <Steven Wu>
2017-02-17 13:43:07 +0100

    fixed error

--- smoelenboek/view/tpls/docent/default.php
+++ smoelenboek/view/tpls/docent/default.php
@@ -4,8 +4,12 @@ include 'includes/menu.php';?>
         <section id='content'>
             <div class="klas-genoot__container">
                 <div class="klas-genoot__mentor-wrapper">
+                <?php if(!empty($mentor)) :?>
                     <img class="klas-genoot__image" src="<?= $mentor->getFoto()?>">
                     <figure class="klas-genoot__name"><?= $mentor->getNaam(); ?></figure>
+                <?php else :?>
+                    <p>Deze klas heeft momenteel geen mentor. Contact Directeur</p>
+                <?php endif;?>
                 </div>
                 <ul class="klas-genoot__list">
                     <?php foreach($klasgenoten as $klasgenoot): ?>
